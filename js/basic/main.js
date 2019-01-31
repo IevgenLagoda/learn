020a812629ebda06de6fbda83a0fafe09624d590
Eugene Lagoda <eugene.lagoda@gmail.com>
2019-01-30 19:55:57 -0800

    Fix

--- js/basic/main.js
+++ js/basic/main.js
@@ -1,3 +1,20 @@
 var a = 1;
 let b = 2;
 const c = 3;
+
+for(let i = 0; i < c; i++) {
+  console.log(i % 1);
+}
+
+let fn = function(a, b) {
+  console.log(a + b);
+}
+
+var book = {
+  title: 'Title',
+  author: "Author",
+  year: '2000',
+  getYear: function() {
+    return this.year;
+  }
+}
