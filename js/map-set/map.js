88ff3c2812712f64c74d3d37d2994aed61fbbc2f
Eugene Lagoda <eugene.lagoda@gmail.com>
2019-02-03 11:39:37 -0800

    Set and Map

--- js/map-set/map.js
+++ js/map-set/map.js
@@ -1,1 +1,15 @@
-let m = new Map();
+let scores = new Map([
+  ['Mike', 100],
+  ['Lili', 120],
+  ['John', 80]
+]);
+
+scores.set('Nick', 110);
+
+console.log(scores.size + ' records');
+
+for([name, score] of scores.entries()) {
+  console.log(`${name} has ${score}`);
+}
+
+scores.forEach((score, name) => console.log(`${name} - ${score}`));
--- js/map-set/set.js
+++ js/map-set/set.js
@@ -1,1 +1,21 @@
-let s = new Set();
+let names = new Set(['John', 'Adam', 'John', 'Lili']);
+console.assert(3 == names.size, "Should be 3, but was " + names.size);
+
+names.add('Mike').add('Adam');
+console.assert(4 == names.size, "Should be 4, but was " + names.size);
+
+console.assert(names.has('Lili'), 'Lili should be there!');
+
+console.log('For of');
+for(let name of names) {
+  console.log(name);
+}
+
+console.log('ForEach');
+names.forEach(name => console.log(name));
+
+console.log('Work with set thru array');
+[...names]
+  .filter(name => name.startsWith('L'))
+  .map(name => name.toUpperCase())
+  .forEach(name => console.log(name));
