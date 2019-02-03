1d8f17eeb306497f2ece774efcaf60809d6d8053
Eugene Lagoda <eugene.lagoda@gmail.com>
2019-02-02 18:47:31 -0800

    Classes and methods

---
+++ js/oop/index.js
@@ -0,0 +1,42 @@
+/**
+ * Classes
+ */
+class Book {
+  constructor(title, author, isdn) {
+    this.title = title;
+    this.author = author;
+    this.isdn = isdn;
+  }
+
+  printIsdn() {
+    console.log(this.isdn);
+  }
+}
+
+class ITBook extends Book {
+  constructor(title, author, isdn, technology) {
+    super(title, author, isdn);
+    this.technology = technology;
+  }
+
+  printTechnology() {
+    console.log(this.technology);
+  }
+
+  get fullTitle() {
+    return `${this.title} by ${this.author}`;
+  }
+  set fullTitle(fullTItle) {
+    let values = fullTItle.split(' by ');
+    this.title = values[0];
+    this.author = values[1];
+  }
+
+}
+
+const book = new Book("Title 1", "Author 1", "1-12323-12323");
+book.printIsdn();
+
+const jsBook = new ITBook("JS book", "JS author", "1-32323-333", "JS");
+jsBook.printIsdn();
+jsBook.printTechnology();
