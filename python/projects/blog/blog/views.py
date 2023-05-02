23e492f65d9823fd160cd89c6681c3fce3c9444e
Ievgen Lagoda <eugene.lagoda@gmail.com>
2023-05-01 20:14:58 -0700

    blog - tags for posts

--- python/projects/blog/blog/views.py
+++ python/projects/blog/blog/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from django.views.generic import ListView
+from taggit.models import Tag
 from .models import Post
 from .forms import EmailPostForm, CommentForm
    
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts,
+                  'tag': tag})
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
@@ -65,8 +90,8 @@ def post_comment(request, post_id):
                             'form': form,
                             'comment': comment})
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
