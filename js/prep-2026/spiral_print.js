f3678b31360d66054dce86a0f421d4d03c1f4307
Ievgen Lagoda <eugene.lagoda@gmail.com>
2026-02-21 19:42:39 -0800

    sync

---
+++ js/prep-2026/spiral_print.js
@@ -0,0 +1,45 @@
+const matrix = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+];
+
+function spriralPrint(matrix) {
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let result = [];
+
+  while (top <= bottom && left <= right) {
+    // Traverse from left to right
+    for (let i = left; i <= right; i++) {
+      result.push(matrix[top][i]);
+    }
+    top++;
+    // Traverse from top to bottom
+    for (let i = top; i <= bottom; i++) {
+      result.push(matrix[i][right]);
+    }
+    right--;
+    // Traverse from right to left
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom--;
+    }
+    // Traverse from bottom to top
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left++;
+    }
+  }
+  return result;
+}
+
+console.log(spriralPrint(matrix));
