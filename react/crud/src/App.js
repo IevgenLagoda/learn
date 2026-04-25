8c374a6f9f27eb312671c781c123e85aab92ed83
Ievgen Lagoda <eugene.lagoda@gmail.com>
2026-04-24 21:04:36 -0700

    First state

--- react/crud/src/App.js
+++ react/crud/src/App.js
@@ -4,23 +4,26 @@ import './App.css';
 import Greating from './components/Greating';
 
 function App() {
+  const user = "Delveoper";
+  const roles = ['user', 'dev', 'admin'];
+  const [isLogged, setIsLogged] = React.useState(false);
+
+  function onClick() {
+    setIsLogged(!isLogged);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greating name="Developer" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Greating name={user} roles={roles} />
+      <button onClick={onClick} value="Click">
+        {isLogged 
+        ? <span>Sign out</span>
+        : <span>Sign in</span>
+        }
+      </button>
     </div>
   );
 }
--- react/crud/src/components/Greating.js
+++ react/crud/src/components/Greating.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Greating(props) {
+    const roles = props.roles.map((role) => 
+        <li key={role}>{role}</li>
+    );
+    
     return (
         <div>
             <h1>Hello {props.name}</h1>
+            <ul>{roles}</ul>
         </div>
     );
 }
